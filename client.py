b4066474598bea92f54c7f97f77755a3e9892207
Вячеслав Бухтояров <fklskadelovoy@gmail.com>
2025-07-21 20:12:22 +1000

    Order List

--- client.py
+++ client.py
@@ -1,12 +1,13 @@
 from tinkoff.invest.services import InstrumentsService
-from tinkoff.invest import InstrumentIdType, InstrumentType
+from tinkoff.invest import InstrumentIdType, InstrumentType, TradingDay, GetDividendsResponse, Dividend, Account
 from tinkoff.invest import Client, Instrument, typedefs, ShareResponse
 from datetime import datetime, timedelta
 import pandas as pd
 
+from settings import CLASS_CODE, REESTR_FIGI
 class MyClient:
     client = None
-
+    iis_id = ""
     def __init__(self, TOKEN: str):
         self.client = Client(TOKEN).__enter__()
     
@@ -14,20 +15,41 @@ class MyClient:
         new_date = fr + timedelta(days=13)
         return self.client.instruments.trading_schedules(exchange="MOEX", from_=datetime.now(), to=new_date)
 
-
-    def saveTradeSchedules(self):
+    def UpdateTradeSchedules(self):
         data = self.getTradingSchedules(datetime.now())
-        df = pd.DataFrame([vars(date) for date in data.exchanges[0].days])
+        properties = ["date", "is_trading_day", "start_time", "end_time"]
+        df = pd.DataFrame({prop: getattr(date, prop) for prop in properties} for date in data.exchanges[0].days)
         df.to_csv("РасписаниеТоргов.csv")
-    
+
     def getFigi(self, tiker: str):
-        self.client.instruments.share_by(InstrumentIdType.INSTRUMENT_ID_TYPE_TICKER, "????", tiker)
-    
+        return self.client.instruments.share_by(id_type=InstrumentIdType.INSTRUMENT_ID_TYPE_TICKER, class_code=CLASS_CODE, id=tiker).instrument.figi
+
+    def getDiv(self, figi: str) -> list[Dividend]:
+        date = datetime.now()
+        return self.client.instruments.get_dividends(figi=figi, from_=date, to=date + timedelta(days=30)).dividends
+
+    def UpdateOrderList(self):
+        divs_data = []
+        for share_figi in REESTR_FIGI:
+            for divs in self.getDiv(share_figi):
+                divs_data.append(divs.last_buy_date)
+
+    def get_RUB_limits(self, id: str):
+        for curr in self.client.operations.get_withdraw_limits(account_id=id).money:
+            if (curr.currency == 'rub'):
+                return curr
+
+    def get_accounts_id(self):
+        id = []
+        for acc in self.client.users.get_accounts().accounts:
+            id.append((acc.id, acc.name))
+        return id
+
     def getShares(self) -> list[Instrument]:
         shares = []
         for acc in self.client.users.get_accounts().accounts:
             print(acc.id)
-            for portfel in self.client.operations.get_portfolio(account_id=acc.id).positions:
+            for portfel in self.client.operations.get_portfolio(account_id=acc.id):
                 #print(portfel.instrument_type)
                 if (portfel.instrument_type == "share"):
                     #print(portfel.ticker)
--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 from datetime import datetime
 from dotenv import load_dotenv
 from client import MyClient
+from settings import REESTR_TICKER
 import os
 
 import pandas as pd
 load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
+IIS_ID = os.getenv("IIS_ID")
 
 client = MyClient(TOKEN)
-
+client.iis_id = IIS_ID
 
 def main():
-    for share in client.getShares():
-        print(share.class_code)
+    print(client.get_RUB_limits(IIS_ID))
 
+
+def pipeline():
+    pass
+
+def updateFigiReestr():
+    reestr = []
+    for ticker in REESTR_TICKER:
+        reestr.append(client.getFigi(ticker))
+    print(reestr)
 
 if __name__ == "__main__":
     main()
